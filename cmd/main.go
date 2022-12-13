0efc4cc476e3d21d5cf88da35573dbf7247f9b61
rv3392 <richalverma00@gmail.com>
2022-12-13 17:00:51 +1000

    Add solution for day one

---
+++ cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/rv3392/advent-of-code-2022/days"
+
+func main() {
+    days.RunDayOne("inputs/day_one.in")
+}
---
+++ days/one.go
@@ -0,0 +1,56 @@
+package days
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+)
+
+/// Runs the solution for day one
+func RunDayOne(in string) {
+    var f = openFile(in)
+    var elves = totalCalPerElf(f)
+    // Take the 3 elves with the highest calories, find their total cals
+    sort.Slice(elves, func (i int, j int) bool {
+        return elves[i] > elves[j]
+    })
+    var total = elves[0] + elves[1] + elves[2]
+
+    fmt.Println("-- Day One --")
+    fmt.Println("(Part One) Highest Cals:", elves[0])
+    fmt.Println("(Part Two) Sum of Highest 3:", total)
+}
+
+func openFile(in string) *os.File {
+    f, err := os.Open(in)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer f.Close()
+
+    return f
+}
+
+func totalCalPerElf(f *os.File) []int {
+    scanner := bufio.NewScanner(f)
+    var elves []int
+    currElf := 0
+    for scanner.Scan() {
+        if scanner.Text() == "" {
+            elves = append(elves, currElf)
+            currElf = 0
+        }
+
+        val, _ := strconv.Atoi(scanner.Text())
+        currElf += val
+    }
+
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+
+    return elves
+}
