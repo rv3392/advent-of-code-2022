4d7c2ae5b2d96c7c7067173ca15d48a0f0039f86
rv3392 <richalverma00@gmail.com>
2022-12-13 17:04:03 +1000

    Run gofmt

--- cmd/main.go
+++ cmd/main.go
@@ -3,5 +3,5 @@ package main
 import "github.com/rv3392/advent-of-code-2022/days"
 
 func main() {
-    days.RunDayOne("inputs/day_one.in")
+	days.RunDayOne("inputs/day_one.in")
 }
--- days/one.go
+++ days/one.go
@@ -11,46 +11,46 @@ import (
 
 /// Runs the solution for day one
 func RunDayOne(in string) {
-    var f = openFile(in)
-    var elves = totalCalPerElf(f)
-    // Take the 3 elves with the highest calories, find their total cals
-    sort.Slice(elves, func (i int, j int) bool {
-        return elves[i] > elves[j]
-    })
-    var total = elves[0] + elves[1] + elves[2]
+	var f = openFile(in)
+	var elves = totalCalPerElf(f)
+	// Take the 3 elves with the highest calories, find their total cals
+	sort.Slice(elves, func(i int, j int) bool {
+		return elves[i] > elves[j]
+	})
+	var total = elves[0] + elves[1] + elves[2]
 
-    fmt.Println("-- Day One --")
-    fmt.Println("(Part One) Highest Cals:", elves[0])
-    fmt.Println("(Part Two) Sum of Highest 3:", total)
+	fmt.Println("-- Day One --")
+	fmt.Println("(Part One) Highest Cals:", elves[0])
+	fmt.Println("(Part Two) Sum of Highest 3:", total)
 }
 
 func openFile(in string) *os.File {
-    f, err := os.Open(in)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer f.Close()
+	f, err := os.Open(in)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
 
-    return f
+	return f
 }
 
 func totalCalPerElf(f *os.File) []int {
-    scanner := bufio.NewScanner(f)
-    var elves []int
-    currElf := 0
-    for scanner.Scan() {
-        if scanner.Text() == "" {
-            elves = append(elves, currElf)
-            currElf = 0
-        }
+	scanner := bufio.NewScanner(f)
+	var elves []int
+	currElf := 0
+	for scanner.Scan() {
+		if scanner.Text() == "" {
+			elves = append(elves, currElf)
+			currElf = 0
+		}
 
-        val, _ := strconv.Atoi(scanner.Text())
-        currElf += val
-    }
+		val, _ := strconv.Atoi(scanner.Text())
+		currElf += val
+	}
 
-    if err := scanner.Err(); err != nil {
-        log.Fatal(err)
-    }
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 
-    return elves
+	return elves
 }
